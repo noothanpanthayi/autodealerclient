662b29b0bb1a4dead3c32aa0a6be144798ba382b
noothanpanthayi <noothankrishnan@outlook.com>
2024-10-20 09:19:29 -0500

    img added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect, Fragment } from "react";
 
-import styles from './home.module.css'
+import styles from "./home.module.css";
 
 function Home() {
-
   type AutoList = {
     _id: string;
     make: string;
@@ -25,11 +24,16 @@ function Home() {
   });
 
   const fetchAuto = async () => {
-    const apiUrl=process.env.REACT_APP_API_URL;
-    const header = await fetch(`${apiUrl}/api/`);
+    // const apiUrl=process.env.REACT_APP_API_URL;
+    console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
+    const apiUrl: any =
+      process.env.NODE_ENV === "production"
+        ? process.env.REACT_APP_API_URL
+        : "/api/";
+    const header = await fetch(apiUrl);
     const autoList = await header.json();
 
-    setState((prevState:any) => {
+    setState((prevState: any) => {
       return {
         ...prevState,
         autoList,
@@ -39,21 +43,43 @@ function Home() {
 
   useEffect(() => {
     fetchAuto();
-  },[]);
+  }, []);
 
-  return <div>
-    <h1>Auto List</h1>
-    {
-        state?.autoList?.map(({_id, make, model, year, price, type})=>{
-            return <div className={card} key={_id}>
-            <div className={title}>{make}</div>
-            <div>{model}</div>
+  return (
+    <div>
+      <h1>Auto List</h1>
+      {state?.autoList?.map(({ _id, make, model, year, price, type }) => {
+        return (
+          <div className={card} key={_id}>
+            <div className={content}>
+              <div>
+                <img
+                  width="200"
+                  alt="car image"
+                  src="https://media.rti.toyota.com/config/pub/3d/vcr/live/vehicle/TOY/2025/camry/2557/455/2ce0ab6f2abf2392135868d33ac516c6bf5199e10cec9bd1183f914e7dbfb58b.png?fit=crop&wid=1200&hei=663&efcview=Exterior&bfc=off&fmt=png-alpha"
+                />
+              </div>
+              <div>
+                <ul>
+                  <li className={title}>{make}</li>
+                  <li className={modelTitle}>{model}</li>
+                  <li className={priceTitle}>
+                    <span className={startTitle}>Starting at:</span>
+                    {price.toLocaleString("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    })}
+                  </li>
+                </ul>
+              </div>
             </div>
-        })
-    }
-  </div>;
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-const {card, title}=styles;
+const { card, title, content, modelTitle, priceTitle, startTitle } = styles;
 
 export default Home;
